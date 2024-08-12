5e05dbb2c61067a3d83e9d6c485ece68f0ded7ad
Jesus-Bobadilla <162188830+Jesus-Bobadilla@users.noreply.github.com>
2024-08-12 16:11:38 -0600

    Update askName.tsx

--- app/afterAuth/askName.tsx
+++ app/afterAuth/askName.tsx
@@ -1,11 +1,38 @@
 import { FormButton } from "@/components/Form/FormButton";
+import { useSession } from "@/context/SessionContext";
 import { supabase } from "@/lib/supabase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Alert, TextInput } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 export default function SignUp() {
   const { styles } = useStyles(stylesheet);
+  const session = useSession();
+  
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [secondLastName, setSecondLastName] = useState("");
+
+  const checkUserProfile = async () => {
+    setIsLoading(true);
+    if (session) {
+      const { data, error } = await supabase
+      .from("profiles")
+      .update({full_name: `${name} ${lastName} ${secondLastName}`})
+      .eq("id", session.user.id)
+      .single();
+      
+      if (error) {
+        Alert.alert("Error", error.message);
+        setIsLoading(false);
+        return;
+      }
+
+      Alert.alert("Éxito", "Perfil actualizado");
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
@@ -14,31 +41,26 @@ export default function SignUp() {
         autoComplete="name"
         style={styles.textInput}
         placeholderTextColor={styles.textInput.placehoolderTextColor}
-      />
-      <TextInput
-        placeholder="Correo electrónico"
-        inputMode="email"
-        autoComplete="username"
-        style={styles.textInput}
-        placeholderTextColor={styles.textInput.placehoolderTextColor}
+        onChangeText={setName}
       />
       <TextInput
         placeholder="Apellido paterno"
         autoComplete="name"
         style={styles.textInput}
         placeholderTextColor={styles.textInput.placehoolderTextColor}
+        onChangeText={setLastName}
       />
       <TextInput
         placeholder="Apellido materno"
         autoComplete="name"
         style={styles.textInput}
         placeholderTextColor={styles.textInput.placehoolderTextColor}
+        onChangeText={setSecondLastName}
       />
       <FormButton
         title="Crear cuenta"
-        onPress={() => {
-          Alert.alert("Crear cuenta");
-        }}
+        onPress={checkUserProfile}
+        isLoading={isLoading}
       />
     </>
   );
