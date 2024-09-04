6deffe0d33be6eb1be8ede8313b740c10488d05c
Jesus-Bobadilla <162188830+Jesus-Bobadilla@users.noreply.github.com>
2024-09-04 18:57:11 +0200

    Solucion Temporal
    o no?

--- app/(protected)/people/[personId]/assignTruck/_layout.tsx
+++ app/(protected)/people/[personId]/assignTruck/_layout.tsx
@@ -1,12 +1,11 @@
 import { router, Stack } from "expo-router";
 import React from "react";
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Layout() {
   const { styles } = useStyles(stylesheet);
-
 
   //Arturo, resuleve ya porfavor JAJAJAJJAJJ. La navegacion no sirve 
 
@@ -32,14 +31,16 @@ export default function Layout() {
           headerShown: true, // Asegura que el header se muestre
           headerLeft: () => (
             <Pressable onPress={() => { }}>
-              <Text style={styles.closeButton} onPress={() => router.back()}>
-                <MaterialIcons 
-                  name="chevron-left" 
-                  size={30} 
-                  color = {styles.closeButton.color}
-                /> 
-                Cerrar
-              </Text>
+              <View style={styles.closeButtonContainer}>
+                <MaterialIcons
+                  name="arrow-back-ios"
+                  size={24}
+                  color={styles.closeButton.color}
+                />
+                <Text style={styles.closeButton} onPress={() => router.back()}>
+                  Atras
+                </Text>
+              </View>
             </Pressable>
           ),
         }}
@@ -53,6 +54,8 @@ const stylesheet = createStyleSheet((theme) => ({
     color: theme.ui.colors.primary,
     fontSize: 18,
     textAlign: "left",
+  },
+  closeButtonContainer: {
     flexDirection: "row",
     alignItems: "center",
     flex: 1,
