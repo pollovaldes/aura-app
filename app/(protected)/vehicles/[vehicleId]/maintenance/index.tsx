d94315548abbdfd40180e74317146092304581f5
Arturo Valdes <36093379+pollovaldes@users.noreply.github.com>
2024-11-17 04:03:26 -0600

    Update index.tsx

--- app/(protected)/vehicles/[vehicleId]/maintenance/index.tsx
+++ app/(protected)/vehicles/[vehicleId]/maintenance/index.tsx
@@ -1,9 +1,28 @@
-import { Text, View } from "react-native";
+import SegmentedControl from "@react-native-segmented-control/segmented-control";
+import { Stack } from "expo-router";
+import { useState } from "react";
+import { ScrollView, View } from "react-native";
 
 export default function index() {
+  const [index, setIndex] = useState(0);
+
   return (
     <View>
-      <Text>Vehicle Maintenance</Text>
+      <Stack.Screen
+        options={{
+          headerLargeTitle: true,
+          title: "Mantenimiento",
+        }}
+      />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <SegmentedControl
+          values={["Solicitudes activas", "Solicitudes resueltas"]}
+          selectedIndex={index}
+          onChange={(event) => {
+            setIndex(event.nativeEvent.selectedSegmentIndex);
+          }}
+        />
+      </ScrollView>
     </View>
   );
 }
