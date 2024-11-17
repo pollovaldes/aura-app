d7b4c41707644455cf11bc5cdb96363d20ef3bf3
Arturo Valdes <36093379+pollovaldes@users.noreply.github.com>
2024-11-17 07:35:30 -0600

    theme fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,23 @@
 import { SessionContextProvider } from "@/context/SessionContext";
 import { supabase } from "@/lib/supabase";
 import { Redirect, Slot } from "expo-router";
-import { ThemeProvider } from "@react-navigation/native";
+import {
+  DarkTheme,
+  ThemeProvider,
+  DefaultTheme,
+} from "@react-navigation/native";
 import { useColorScheme } from "react-native";
-import { darkTheme, lightTheme } from "@/style/themes";
 import { usePathname } from "expo-router/build/hooks";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const path = usePathname();
 
+  const currentTheme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
+
   return (
     <SessionContextProvider supabaseClient={supabase}>
-      <ThemeProvider
-        value={colorScheme === "dark" ? darkTheme.ui : lightTheme.ui}
-      >
+      <ThemeProvider value={currentTheme}>
         {path === "/" && <Redirect href="/auth" />}
         <Slot />
       </ThemeProvider>
