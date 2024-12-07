e74c9b2e61b0634230f1f1c3bf33d4da95556b30
Arturo Valdes <36093379+pollovaldes@users.noreply.github.com>
2024-12-06 22:03:23 -0600

    Multiple file upload initial work

--- components/vehicles/modals/AddMaintenance.tsx
+++ components/vehicles/modals/AddMaintenance.tsx
@@ -5,10 +5,17 @@ import FilePickerMenu from "@/components/maintenance/filePickerMenu";
 import { supabase } from "@/lib/supabase";
 import { User } from "@/types/User";
 import { Vehicle } from "@/types/Vehicle";
-import { ImagePlus, Plus, Trash2 } from "lucide-react-native";
+import { DocumentPickerAsset } from "expo-document-picker";
+import { ArrowUpFromLine, File, Plus, Trash2, X } from "lucide-react-native";
 import { useState } from "react";
 import { Platform, Text, TouchableOpacity, View } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
+import * as ImagePicker from "expo-image-picker";
+import * as DocumentPicker from "expo-document-picker";
+import * as FileSystem from "expo-file-system";
+import * as Crypto from "expo-crypto";
+import { Maintenance } from "@/hooks/useMaintenance";
+import { decode } from "base64-arraybuffer";
 
 interface addMaintenanceModalProps {
   closeModal: () => void;
@@ -20,6 +27,7 @@ interface addMaintenanceModalProps {
 interface FileItem {
   localId: string;
   description: string;
+  document?: DocumentPickerAsset | ImagePicker.ImagePickerAsset | null;
 }
 
 export default function AddMaintenance({
@@ -35,6 +43,10 @@ export default function AddMaintenance({
   const [requestTitle, setRequestTitle] = useState("");
   const [requestDescription, setRequestDescription] = useState("");
   const [isUploading, setIsUploading] = useState(false);
+
+  const [newMaintenance, setNewMaintenance] = useState<Maintenance | null>(
+    null
+  );
 
   // ================== File picker ==================
 
@@ -65,6 +77,57 @@ export default function AddMaintenance({
 
   // ================== File picker ==================
 
+  const uploadDocumentToStorage = async (
+    localId: string,
+    maintenanceId: string
+  ): Promise<boolean> => {
+    const file = fileViews.find((file) => file.localId === localId);
+
+    if (!file || !file.document) {
+      alert(
+        `No se puede subir el archivo con id: ${localId} porque no se ha seleccionado ninguno`
+      );
+      return false;
+    }
+
+    let fileData;
+    if (Platform.OS === "web") {
+      fileData = file.document.file;
+    } else {
+      fileData = await FileSystem.readAsStringAsync(file.document.uri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      console.log("File data: ", fileData);
+      fileData = decode(fileData);
+      console.log("Decoded file data: ", fileData);
+    }
+
+    if (!fileData) {
+      alert("No se pudo obtener datos del archivo seleccionado");
+      return false;
+    }
+
+    const fileName = `${vehicle.id}/${maintenanceId}/${Crypto.randomUUID()}`;
+    console.log("Nombre archivo ", fileName);
+
+    const { data, error } = await supabase.storage
+      .from("maintenance_files")
+      .upload(fileName, fileData, {
+        contentType: file.document.mimeType,
+      });
+
+    console.log("Data: ", data);
+
+    if (error) {
+      alert(
+        `Error subiendo archivo con id: ${localId}\nMensaje de error: ${error.message}`
+      );
+      return false;
+    }
+
+    return true;
+  };
+
   const uploadRequest = async (
     vehicle_id: string,
     issued_by: string,
@@ -72,40 +135,135 @@ export default function AddMaintenance({
     title: string,
     description: string
   ) => {
-    try {
-      setIsUploading(true);
+    setIsUploading(true);
 
-      const { data, error } = await supabase
-        .from("maintenance")
-        .insert([
-          {
-            vehicle_id,
-            issued_by,
-            issued_datetime,
-            title,
-            description,
-            status: "PENDING_REVISION", // Default status
-          },
-        ])
-        .select();
+    const { data, error } = await supabase
+      .from("maintenance")
+      .insert([
+        {
+          vehicle_id,
+          issued_by,
+          issued_datetime,
+          title,
+          description,
+          status: "PENDING_REVISION", // Default status
+        },
+      ])
+      .select();
 
-      if (!error && data) {
-        setIsUploading(false);
-        fetchMaintenance();
-        closeModal();
-        return data;
-      } else {
-        alert(`¡Ocurrió un error!\n${error.message}`);
-        setIsUploading(false);
+    if (!error && data) {
+      const maintenanceId = data[0].id;
+
+      console.log("Antes del for");
+
+      for (const file of fileViews) {
+        const success = await uploadDocumentToStorage(
+          file.localId,
+          maintenanceId
+        );
+        if (!success) {
+          alert("Ocurrió un error subiendo el archivo con id: " + file.localId);
+          setIsUploading(false);
+          return;
+        }
+        console.log("Fin de iteración");
       }
-    } catch (error) {
-      alert(`¡Ocurrió un error en la función uploadRequest!\n${error}`);
-      throw error;
+
+      console.log("Despues del for");
+
+      setIsUploading(false);
+      fetchMaintenance();
+      closeModal();
+      return data;
+    } else {
+      alert(`¡Ocurrió un error!\n${error.message}`);
+      setIsUploading(false);
+    }
+
+    alert(`¡Ocurrió un error en la función uploadRequest!\n${error}`);
+  };
+
+  const handleSelectFile = async (key: string, localId: string) => {
+    if (key === "image") {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ["images", "videos"],
+        allowsEditing: true,
+        quality: 0.35,
+        selectionLimit: 1,
+        videoMaxDuration: 60,
+        base64: true,
+      });
+
+      if (result.canceled) {
+        alert("La selección de imagen fue cancelada");
+
+        setFileViews((prev) =>
+          prev.map((file) =>
+            file.localId === localId ? { ...file, document: null } : file
+          )
+        );
+
+        return;
+      }
+
+      if (result.assets) {
+        setFileViews((prev) =>
+          prev.map((file) =>
+            file.localId === localId
+              ? { ...file, document: result.assets[0] }
+              : file
+          )
+        );
+
+        //IMAGES
+        console.log(result.assets[0]);
+      } else {
+        setFileViews((prev) =>
+          prev.map((file) =>
+            file.localId === localId ? { ...file, document: null } : file
+          )
+        );
+      }
+    } else if (key === "file" || key === "web") {
+      const result = await DocumentPicker.getDocumentAsync({
+        type: "*/*",
+        copyToCacheDirectory: true,
+      });
+
+      if (result.canceled) {
+        alert("La selección de archivo fue cancelada");
+
+        setFileViews((prev) =>
+          prev.map((file) =>
+            file.localId === localId ? { ...file, document: null } : file
+          )
+        );
+
+        return;
+      }
+
+      if (result.assets) {
+        setFileViews((prev) =>
+          prev.map((file) =>
+            file.localId === localId
+              ? { ...file, document: result.assets[0] }
+              : file
+          )
+        );
+        //FILES
+        console.log(result.assets[0]);
+      } else {
+        setFileViews((prev) =>
+          prev.map((file) =>
+            file.localId === localId ? { ...file, document: null } : file
+          )
+        );
+      }
     }
   };
 
-  const handleDropDownSelect = (key: string) => {
-    alert(`Seleccionaste ${key}`);
+  const handleDropDownSelect = (key: string, localId: string) => {
+    handleSelectFile(key, localId);
   };
 
   return (
@@ -156,35 +314,76 @@ export default function AddMaintenance({
                     { key: "image", title: "Seleccionar desde la galeria" },
                     { key: "file", title: "Seleccionar desde archivos" },
                   ]}
-                  onSelect={handleDropDownSelect}
+                  onSelect={(key) => handleDropDownSelect(key, item.localId)}
                   trigger={
-                    <View style={styles.selectFileContainer}>
-                      <ImagePlus color={styles.fileIcon.color} size={50} />
-                      <Text
-                        style={styles.fileText}
-                        ellipsizeMode="middle"
-                        numberOfLines={2}
-                      >
-                        {"Seleccionar\narchivo"}
-                      </Text>
-                    </View>
+                    <FormButton
+                      title="Seleccionar archivo"
+                      onPress={() => {}}
+                      icon={() => (
+                        <ArrowUpFromLine
+                          color={styles.selectFileButtonIcon.color}
+                        />
+                      )}
+                    />
                   }
                 />
               )}
 
               {Platform.OS === "web" && (
-                <TouchableOpacity>
-                  <View style={styles.selectFileContainer}>
-                    <ImagePlus color={styles.fileIcon.color} size={50} />
-                    <Text
-                      style={styles.fileText}
-                      ellipsizeMode="middle"
-                      numberOfLines={2}
+                <FormButton
+                  title="Seleccionar archivo"
+                  onPress={() => handleSelectFile("web", item.localId)}
+                  icon={() => (
+                    <ArrowUpFromLine
+                      color={styles.selectFileButtonIcon.color}
+                    />
+                  )}
+                />
+              )}
+              {item.document && (
+                <View style={styles.filePreviewContainer}>
+                  <View>
+                    <TouchableOpacity
+                      onPress={() =>
+                        setFileViews((prev) =>
+                          prev.map((file) =>
+                            file.localId === item.localId
+                              ? { ...file, document: null }
+                              : file
+                          )
+                        )
+                      }
                     >
-                      {"Seleccionar\narchivo"}
-                    </Text>
+                      <X color={styles.fileIcon.color} />
+                    </TouchableOpacity>
                   </View>
-                </TouchableOpacity>
+                  <View style={{ flexDirection: "row", gap: 6 }}>
+                    <File color={styles.fileIcon.color} size={50} />
+                    <View
+                      style={{
+                        flexDirection: "column",
+                        justifyContent: "space-around",
+                        width: "75%",
+                      }}
+                    >
+                      <Text
+                        style={styles.fileText}
+                        ellipsizeMode="middle"
+                        numberOfLines={2}
+                      >
+                        {item.document && "name" in item.document
+                          ? item.document.name
+                          : "Archivo seleccionado desde la galería"}
+                      </Text>
+                      <Text style={styles.fileSubtitle}>
+                        {item.document && "size" in item.document
+                          ? ((item.document.size ?? 0) / 1000000).toFixed(2) +
+                            " MB"
+                          : "Tamaño desconocido"}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
               )}
               <FormInput
                 description="Descripción del archivo"
@@ -300,11 +499,24 @@ const stylesheet = createStyleSheet((theme) => ({
     padding: 12,
   },
   fileIcon: {
-    color: theme.ui.colors.primary,
+    color: theme.textPresets.main,
   },
   fileText: {
     color: theme.textPresets.main,
     fontSize: 16,
-    textAlign: "center",
+  },
+  filePreviewContainer: {
+    borderColor: theme.ui.colors.border,
+    borderWidth: 1,
+    borderRadius: 6,
+    padding: 12,
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+  },
+  selectFileButtonIcon: {
+    color: theme.textPresets.inverted,
+  },
+  fileSubtitle: {
+    color: theme.textPresets.main,
   },
 }));
