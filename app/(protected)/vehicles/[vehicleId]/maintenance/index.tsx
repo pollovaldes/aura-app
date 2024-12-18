7929516f0a1ccd35b13e0516f526d0105aa8672f
Arturo Valdes <36093379+pollovaldes@users.noreply.github.com>
2024-12-18 16:24:29 -0600

    updated manintenance list

--- app/(protected)/vehicles/[vehicleId]/maintenance/index.tsx
+++ app/(protected)/vehicles/[vehicleId]/maintenance/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import EmptyScreen from "@/components/dataStates/EmptyScreen";
 import ErrorScreen from "@/components/dataStates/ErrorScreen";
 import { FetchingIndicator } from "@/components/dataStates/FetchingIndicator";
@@ -9,30 +9,30 @@ import AddMaintenance from "@/components/vehicles/modals/AddMaintenance";
 import useVehicle from "@/hooks/truckHooks/useVehicle";
 import useMaintenance from "@/hooks/useMaintenance";
 import useProfile from "@/hooks/useProfile";
-import { User } from "@/types/User";
 import SegmentedControl from "@react-native-segmented-control/segmented-control";
 import { useHeaderHeight } from "@react-navigation/elements";
-import { Link, Stack, useLocalSearchParams } from "expo-router";
-import { ChevronRight, Plus } from "lucide-react-native";
-import { useState } from "react";
-import { FlatList, Platform, Pressable, Text, View } from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { Plus, ChevronRight } from "lucide-react-native";
+import { FlatList, Platform, Text, View } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 import { ActionButtonGroup } from "@/components/actionButton/ActionButtonGroup";
 import { ActionButton } from "@/components/actionButton/ActionButton";
+import { SimpleList } from "@/components/simpleList/SimpleList";
 
 type ModalType = "create_maintenance_record" | null;
 
-export default function Index() {
+export default function MaintenanceRequests() {
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
+  const [activeModal, setActiveModal] = useState<ModalType>(null);
   const { styles } = useStyles(stylesheet);
   const { vehicles, vehiclesAreLoading, fetchVehicles } = useVehicle();
   const { profile, isProfileLoading, fetchProfile } = useProfile();
   const { vehicleId } = useLocalSearchParams<{ vehicleId: string }>();
   const { maintenanceRecords, areMaintenanceRecordsLoading, fetchMaintenance } =
     useMaintenance(vehicleId);
-  const [activeModal, setActiveModal] = useState<ModalType>(null);
-  const closeModal = () => setActiveModal(null);
   const headerHeight = useHeaderHeight();
+
+  const closeModal = () => setActiveModal(null);
 
   if (isProfileLoading || vehiclesAreLoading || areMaintenanceRecordsLoading) {
     return (
@@ -41,161 +41,98 @@ export default function Index() {
           options={{
             title: "Cargando...",
             headerLargeTitle: false,
-            headerRight: undefined,
           }}
         />
-        <FetchingIndicator
-          caption={
-            isProfileLoading
-              ? "Cargando perfil"
-              : vehiclesAreLoading
-                ? "Cargando vehículos"
-                : "Cargando registros de mantenimiento"
-          }
-        />
+        <FetchingIndicator caption="Cargando datos..." />
       </>
     );
   }
 
   if (!profile) {
     return (
-      <>
-        <Stack.Screen
-          options={{
-            title: "Error",
-            headerLargeTitle: false,
-            headerRight: undefined,
-          }}
-        />
-        <ErrorScreen
-          caption="Ocurrió un error al recuperar tu perfil"
-          buttonCaption="Reintentar"
-          retryFunction={fetchProfile}
-        />
-      </>
+      <ErrorScreen
+        caption="Ocurrió un error al recuperar tu perfil"
+        buttonCaption="Reintentar"
+        retryFunction={fetchProfile}
+      />
     );
   }
 
-  if (vehicles === null) {
+  if (!vehicles) {
     return (
-      <>
-        <Stack.Screen
-          options={{
-            title: "Recurso inaccesible",
-            headerLargeTitle: false,
-            headerRight: undefined,
-          }}
-        />
-        <ErrorScreen
-          caption={`Ocurrió un error y no \npudimos cargar los vehículos`}
-          buttonCaption="Reintentar"
-          retryFunction={fetchVehicles}
-        />
-      </>
+      <ErrorScreen
+        caption="Ocurrió un error al cargar los vehículos"
+        buttonCaption="Reintentar"
+        retryFunction={fetchVehicles}
+      />
     );
   }
 
-  const vehicle = vehicles.find((Vehicle) => Vehicle.id === vehicleId);
+  const vehicle = vehicles.find((v) => v.id === vehicleId);
 
   if (!vehicle) {
     return (
-      <>
-        <Stack.Screen
-          options={{
-            title: "Recurso inaccesible",
-            headerRight: undefined,
-            headerLargeTitle: false,
-            headerTitle: undefined,
-          }}
-        />
-        <UnauthorizedScreen
-          caption="No tienes acceso a este recurso."
-          buttonCaption="Reintentar"
-          retryFunction={fetchVehicles}
-        />
-      </>
+      <UnauthorizedScreen
+        caption="No tienes acceso a este recurso."
+        buttonCaption="Reintentar"
+        retryFunction={fetchVehicles}
+      />
     );
   }
 
   if (!maintenanceRecords) {
     return (
-      <>
-        <Stack.Screen
-          options={{
-            title: "Error",
-            headerLargeTitle: false,
-            headerRight: undefined,
-          }}
-        />
-        <UnauthorizedScreen
-          caption={`Ocurrió un error y no pudimos \ncargar las solicitudes de mantenimiento`}
-          buttonCaption="Reintentar"
-          retryFunction={fetchMaintenance}
-        />
-      </>
+      <ErrorScreen
+        caption="Ocurrió un error al cargar las solicitudes de mantenimiento"
+        buttonCaption="Reintentar"
+        retryFunction={fetchMaintenance}
+      />
     );
   }
 
-  const currentVechicleMaintenanceRecords = maintenanceRecords.filter(
-    (record) => record.vehicle_id === vehicle.id
-  );
+  const filteredMaintenanceRecords = maintenanceRecords.filter((record) => {
+    switch (currentTabIndex) {
+      case 1:
+        return record.status === "PENDING_REVISION";
+      case 2:
+        return record.status === "IN_REVISION";
+      case 3:
+        return record.status === "SOLVED";
+      default:
+        return true;
+    }
+  });
 
-  if (currentVechicleMaintenanceRecords.length != 0) {
-    const uuids = currentVechicleMaintenanceRecords.map(
-      (record) => record.issued_by
-    );
-  }
-
-  function formatDate(dateString: string, prefix: string) {
+  const formatDate = (dateString: string, prefix: string) => {
     const date = new Date(dateString);
-
     const day = date.getDate();
-    const month = date.toLocaleString("es", { month: "long" }); // Get month in Spanish
+    const month = date.toLocaleString("es", { month: "long" });
     const year = date.getFullYear();
-
-    const hours = date.getHours().toString().padStart(2, "0"); // Add leading zero
-    const minutes = date.getMinutes().toString().padStart(2, "0"); // Add leading zero
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
 
     return `${prefix}${day} de ${month} del ${year} a las ${hours}:${minutes} horas`;
-  }
+  };
 
-  const canEdit =
-    profile.role === "ADMIN" ||
-    profile.role === "OWNER" ||
-    profile.role === "DRIVER";
+  const canEdit = ["ADMIN", "OWNER", "DRIVER"].includes(profile.role);
 
   const statesConfig = {
     IN_REVISION: {
       text: "En revisión",
-      backgroundColor: "#e3f2fd", // Azul claro
-      textColor: "#1e88e5", // Azul
+      backgroundColor: "#e3f2fd",
+      textColor: "#1e88e5",
     },
     PENDING_REVISION: {
       text: "Recibido",
-      backgroundColor: "#fff3e0", // Naranja claro
-      textColor: "#ef6c00", // Naranja
+      backgroundColor: "#fff3e0",
+      textColor: "#ef6c00",
     },
     SOLVED: {
       text: "Resuelto",
-      backgroundColor: "#e8f5e9", // Verde claro
-      textColor: "#2e7d32", // Verde
+      backgroundColor: "#e8f5e9",
+      textColor: "#2e7d32",
     },
   };
-
-  const filteredMaintenanceRecords = currentVechicleMaintenanceRecords.filter(
-    (record) => {
-      switch (currentTabIndex) {
-        case 1:
-          return record.status === "PENDING_REVISION";
-        case 2:
-          return record.status === "IN_REVISION";
-        case 3:
-          return record.status === "SOLVED";
-        default:
-          return true;
-      }
-    }
-  );
 
   return (
     <>
@@ -212,151 +149,72 @@ export default function Index() {
           />
         </View>
       </Modal>
-      {currentVechicleMaintenanceRecords.length === 0 ? (
-        <>
-          <Stack.Screen
-            options={{
-              title: "Solicitudes de mantenimiento",
-              headerLargeTitle: false,
-              headerTitle: undefined,
-              headerRight: () => (
-                <ActionButtonGroup>
-                  <ActionButton
-                    Icon={Plus}
-                    onPress={() => setActiveModal("create_maintenance_record")}
-                    show={canEdit}
-                  />
-                </ActionButtonGroup>
-              ),
-            }}
-          />
-          <EmptyScreen
-            caption={"No hay solicitudes de mantenimiento\npara este vehículo"}
-            retryFunction={fetchMaintenance}
-            buttonCaption="Reintentar"
-          />
-        </>
-      ) : (
-        <>
-          <Stack.Screen
-            options={{
-              title: "Solicitudes de mantenimiento",
-              headerLargeTitle: false,
-              headerTitle: undefined,
-              headerRight: () => (
-                <ActionButtonGroup>
-                  <ActionButton
-                    Icon={Plus}
-                    onPress={() => setActiveModal("create_maintenance_record")}
-                    show={canEdit}
-                  />
-                </ActionButtonGroup>
-              ),
-            }}
-          />
-          <SegmentedControl
-            values={["Todo", "Recibidos", "En revisión", "Resuletas"]}
-            selectedIndex={currentTabIndex}
-            onChange={(event) =>
-              setCurrentTabIndex(event.nativeEvent.selectedSegmentIndex)
+
+      <Stack.Screen
+        options={{
+          title: "Solicitudes de mantenimiento",
+          headerLargeTitle: false,
+          headerRight: () => (
+            <ActionButtonGroup>
+              <ActionButton
+                Icon={Plus}
+                onPress={() => setActiveModal("create_maintenance_record")}
+                show={canEdit}
+              />
+            </ActionButtonGroup>
+          ),
+        }}
+      />
+
+      <SegmentedControl
+        values={["Todo", "Recibidos", "En revisión", "Resueltas"]}
+        selectedIndex={currentTabIndex}
+        onChange={(event) =>
+          setCurrentTabIndex(event.nativeEvent.selectedSegmentIndex)
+        }
+        style={[
+          styles.segmentedControl,
+          { marginTop: Platform.OS === "ios" ? headerHeight + 6 : 6 },
+        ]}
+      />
+
+      <FlatList
+        data={filteredMaintenanceRecords}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <SimpleList
+            href={`/vehicles/${vehicleId}/maintenance/${item.id}`}
+            leading={
+              <>
+                <StatusChip status={item.status} statesConfig={statesConfig} />
+              </>
             }
-            style={[
-              styles.segmentedControl,
-              { marginTop: Platform.OS === "ios" ? headerHeight + 6 : 6 },
-            ]}
-          />
-          <FlatList
-            refreshing={
-              vehiclesAreLoading ||
-              areMaintenanceRecordsLoading ||
-              isProfileLoading
+            content={
+              <>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.subtitle}>
+                  {(() => {
+                    const issuedDate = item.issued_datetime
+                      ? formatDate(item.issued_datetime, "el ")
+                      : "una fecha desconocida";
+                    return `Creado el ${issuedDate}`;
+                  })()}
+                </Text>
+              </>
             }
-            onRefresh={() => {
-              fetchVehicles();
-              fetchProfile();
-              fetchMaintenance();
-            }}
-            contentInsetAdjustmentBehavior="automatic"
-            data={filteredMaintenanceRecords}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <Link
-                href={{
-                  pathname: `/vehicles/${vehicleId}/maintenance/${item.id}`,
-                }}
-                asChild
-              >
-                <Pressable>
-                  <View style={styles.container}>
-                    <View style={styles.contentContainer}>
-                      <View
-                        style={{ flexDirection: "row", alignItems: "center" }}
-                      >
-                        <StatusChip
-                          status={item.status}
-                          statesConfig={statesConfig}
-                        />
-                        <Text style={styles.title}>{item.title}</Text>
-                      </View>
-                      <View>
-                        <Text style={styles.subtitle}>
-                          {(() => {
-                            const {
-                              issued_by,
-                              issued_datetime,
-                              resolved_by,
-                              resolved_datetime,
-                              status,
-                            } = item;
-
-                            const getFullName = (user: User | null): string => {
-                              return user
-                                ? `${user.name} ${user.father_last_name} ${user.mother_last_name}`.trim()
-                                : "un usuario desconocido";
-                            };
-
-                            const issuedByName = getFullName(issued_by);
-                            const issuedDate = issued_datetime
-                              ? formatDate(issued_datetime, "el ")
-                              : "una fecha desconocida";
-
-                            if (status === "SOLVED") {
-                              const resolvedByName = getFullName(resolved_by!);
-                              const resolvedDate = resolved_datetime
-                                ? formatDate(resolved_datetime, "el ")
-                                : "una fecha desconocida";
-
-                              return `Esta solicitud fue hecha por ${issuedByName} ${issuedDate} y fue resuelta ${resolvedDate} por ${resolvedByName}`;
-                            }
-
-                            return `Esta solicitud fue hecha por ${issuedByName} ${issuedDate}`;
-                          })()}
-                        </Text>
-                      </View>
-                    </View>
-                    <View style={styles.chevronView}>
-                      <ChevronRight color={styles.chevron.color} />
-                    </View>
-                  </View>
-                </Pressable>
-              </Link>
-            )}
-            ListEmptyComponent={() => (
-              <View
-                style={{
-                  height: 250,
-                }}
-              >
-                <EmptyScreen
-                  caption={
-                    "No hay solicitudes de mantenimiento\ncon este filtro"
-                  }
-                />
-              </View>
-            )}
+            trailing={<ChevronRight color={styles.chevron.color} />}
           />
-        </>
-      )}
+        )}
+        ListEmptyComponent={() => (
+          <EmptyScreen caption="No hay solicitudes de mantenimiento con este filtro" />
+        )}
+        refreshing={vehiclesAreLoading || areMaintenanceRecordsLoading}
+        onRefresh={() => {
+          fetchVehicles();
+          fetchProfile();
+          fetchMaintenance();
+        }}
+      />
     </>
   );
 }
@@ -366,40 +224,6 @@ const stylesheet = createStyleSheet((theme) => ({
     width: "97%",
     margin: "auto",
     marginBottom: 6,
-  },
-  container: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: theme.ui.colors.border,
-    padding: 12,
-  },
-  contentContainer: {
-    gap: 6,
-    flexDirection: "column",
-    flex: 1,
-    marginHorizontal: 12,
-  },
-  title: {
-    fontSize: 17,
-    fontWeight: "bold",
-    paddingLeft: 10,
-    color: theme.textPresets.main,
-    marginRight: 120,
-  },
-  subtitle: {
-    color: theme.textPresets.subtitle,
-    fontSize: 15,
-  },
-  plusIcon: {
-    fontSize: 16,
-    color: theme.headerButtons.color,
-  },
-  chevron: {
-    color: theme.textPresets.subtitle,
-  },
-  chevronView: {
-    paddingRight: 12,
   },
   modalContainer: {
     width: "100%",
@@ -414,15 +238,16 @@ const stylesheet = createStyleSheet((theme) => ({
     fontSize: 18,
     textAlign: "right",
   },
-  status: {
-    marginRight: 12,
-    paddingHorizontal: 12,
-    paddingVertical: 6,
-    borderRadius: 20,
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: theme.textPresets.main,
   },
-  statusText: {
-    fontSize: 12,
-    fontWeight: "600",
-    textAlign: "center",
+  subtitle: {
+    color: theme.textPresets.subtitle,
+    fontSize: 15,
+  },
+  chevron: {
+    color: theme.textPresets.subtitle,
   },
 }));
