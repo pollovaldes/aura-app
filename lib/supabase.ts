dbbf8c3631bec88e55a01fe1b4339d0ae546baba
Arturo Valdes <36093379+pollovaldes@users.noreply.github.com>
2024-12-19 05:25:17 -0600

    Supabase types

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,12 +2,13 @@ import { AppState } from "react-native";
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/database.types";
 
 const supabaseUrl = "https://wkkfbhlvghhrscihbdev.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indra2ZiaGx2Z2hocnNjaWhiZGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxMjQ0NjAsImV4cCI6MjAzOTcwMDQ2MH0.9LcfHIKQe35j69ZubuK-HFv69ANb60LZ0FTdDxWR2Ts";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
