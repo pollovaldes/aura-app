99caca3840de87dea0f80bddf615a54db17ca195
Arturo Valdes <36093379+pollovaldes@users.noreply.github.com>
2024-12-25 14:03:02 -0600

    updated some styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,22 +3,21 @@ import { supabase } from "@/lib/supabase";
 import { Redirect, Slot } from "expo-router";
 import { useColorScheme } from "react-native";
 import { useNavigationContainerRef, usePathname } from "expo-router/build/hooks";
-import { ThemeProvider, DarkTheme, DefaultTheme } from "@react-navigation/native";
+import { ThemeProvider } from "@react-navigation/native";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import Toast from "react-native-toast-message";
 import { toastConfig } from "@/components/toast/ToastConfig";
 import { useReactNavigationDevTools } from "@dev-plugins/react-navigation";
-import { useLogger } from "@react-navigation/devtools";
+import { darkTheme, lightTheme } from "@/style/themes";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const path = usePathname();
-  const currentTheme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
+  const currentTheme = colorScheme === "dark" ? darkTheme.ui : lightTheme.ui;
 
   const navigationRef = useNavigationContainerRef();
   useReactNavigationDevTools(navigationRef);
-  //useLogger(navigationRef);
 
   useEffect(() => {
     SplashScreen.setOptions({
--- style/themes.ts
+++ style/themes.ts
@@ -1,60 +1,5 @@
-// export const lightTheme = {
-//   colors: {
-//     text: {
-//       main: "#1c1c1e",
-//       mainInverted: "#e5e5e7",
-//       subtitle: "#8a8a8e",
-//     },
-//     brands: {
-//       inverted: "#000000",
-//       phone: "#3478f6",
-//     },
-//     backgroundMain: "#ffffff",
-//     backgroundSecondary: "#fafafa",
-//     backgroundTertiary: "#f5f5f7",
-//     backgroundSelected: "#e7e7e9",
-//     outline: "#d8d8d8",
-//     inverted: "#000000",
-//     primary: "#3478f6",
-//     imageOverlay: "rgba(0,0,0, 0.15)",
-//   },
-//   margins: {
-//     sm: 2,
-//     md: 4,
-//     lg: 8,
-//     xl: 12,
-//   },
-// } as const;
-
-import { Platform, useWindowDimensions } from "react-native";
-
-// export const darkTheme = {
-//   colors: {
-//     text: {
-//       main: "#e5e5e7",
-//       mainInverted: "#1c1c1e",
-//       subtitle: "#98989e",
-//     },
-//     brands: {
-//       inverted: "#ffffff",
-//       phone: "#7ab6ff",
-//     },
-//     backgroundMain: "#000000",
-//     backgroundSecondary: "#0d0d0d",
-//     backgroundTertiary: "#141414",
-//     backgroundSelected: "#181816",
-//     outline: "#272729",
-//     inverted: "#ffffff",
-//     primary: "#3b82f7",
-//     imageOverlay: "rgba(0,0,0, 0.45)",
-//   },
-//   margins: {
-//     sm: 2,
-//     md: 4,
-//     lg: 8,
-//     xl: 12,
-//   },
-// } as const;
+import { Platform } from "react-native";
+import { fonts } from "@react-navigation/native/src/theming/fonts";
 
 export const colorPalette = {
   neutral: {
@@ -174,6 +119,7 @@ export const lightTheme = {
       border: "rgb(216, 216, 216)",
       notification: "rgb(255, 59, 48)",
     },
+    fonts,
   },
   colors: {
     inverted: "#000000",
@@ -242,6 +188,7 @@ export const darkTheme = {
       border: "rgb(39, 39, 41)",
       notification: "rgb(255, 69, 58)",
     },
+    fonts,
   },
   colors: {
     inverted: "#ffffff",
