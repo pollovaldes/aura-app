a8d797f67657301cf29e26f97c74f572d182502c
Arturo Valdes <36093379+pollovaldes@users.noreply.github.com>
2024-12-25 19:06:15 -0600

    more color updates (quería ser feliz un día y poner algo bonito antes de seguir con el resto de la app que sí importa)

--- style/accentColor.ts
+++ style/accentColor.ts
@@ -1,5 +1,20 @@
-// accentColors.ts
-export type AccentColorName = "blue" | "purple" | "pink" | "red" | "orange" | "yellow" | "green" | "grey";
+export type AccentColorName =
+  | "blue"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "teal"
+  | "indigo"
+  | "purple"
+  | "brown"
+  | "gray"
+  | "cyan"
+  | "pink"
+  | "lime"
+  | "gold"
+  | "peach"
+  | "crimson";
 
 interface AccentColorSet {
   light: string;
@@ -12,32 +27,64 @@ export const ACCENT_COLORS: Record<AccentColorName, AccentColorSet> = {
     light: "#007AFF",
     dark: "#0A84FF",
   },
-  purple: {
-    light: "#5856D6",
-    dark: "#5E5CE6",
-  },
-  pink: {
-    light: "#FF2D55",
-    dark: "#FF375F",
-  },
   red: {
-    light: "#FF3B30",
-    dark: "#FF453A",
+    light: "#eb4e3d",
+    dark: "#eb5545",
   },
   orange: {
-    light: "#FF9500",
-    dark: "#FF9F0A",
+    light: "#f19a37",
+    dark: "#f2a33c",
   },
   yellow: {
-    light: "#FFCC00",
-    dark: "#FFD60A",
+    light: "#f7ce46",
+    dark: "#f9d84a",
   },
   green: {
-    light: "#34C759",
-    dark: "#32D74B",
+    light: "#63ca56",
+    dark: "#6bd45f",
   },
-  grey: {
-    light: "#8E8E93",
-    dark: "#8E8E93",
+  teal: {
+    light: "#6eabc1",
+    dark: "#80c2d9",
+  },
+  indigo: {
+    light: "#5856cf",
+    dark: "#5e5cde",
+  },
+  purple: {
+    light: "#a357d7",
+    dark: "#b260ea",
+  },
+  brown: {
+    light: "#9d8563",
+    dark: "#a78f6d",
+  },
+  gray: {
+    light: "#8e8e93",
+    dark: "#98989d",
+  },
+  cyan: {
+    light: "#4cb4d9",
+    dark: "#5fc8ed",
+  },
+  pink: {
+    light: "#ff5e8a",
+    dark: "#ff7098",
+  },
+  lime: {
+    light: "#b7e046",
+    dark: "#c4ea5b",
+  },
+  gold: {
+    light: "#f5c242",
+    dark: "#f7cc51",
+  },
+  peach: {
+    light: "#f4a783",
+    dark: "#f6b291",
+  },
+  crimson: {
+    light: "#d5465b",
+    dark: "#e15767",
   },
 };
